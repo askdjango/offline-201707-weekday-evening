112176c543bbd34bc6668bc281d513ebee0830b3
Chinseok Lee <allieuslee@gmail.com>
2017-07-10 21:43:57 +0900

    dojo.views.mysum에서 다수 숫자 처리

--- dojo/views.py
+++ dojo/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def mysum(request, x):
-    return HttpResponse(int(x))
+def mysum(request, numbers):
+    total = sum(int(i or 0) for i in numbers.split('/'))  # generator expression
+    return HttpResponse(total)
 
