3ecea9eb3a4a2ef8d1d4944fae0822c07728fd96
Chinseok Lee <allieuslee@gmail.com>
2017-07-10 21:52:30 +0900

    dojo.views.hello 뷰 구현

--- dojo/views.py
+++ dojo/views.py
@@ -6,3 +6,7 @@ def mysum(request, numbers):
     total = sum(int(i or 0) for i in numbers.split('/'))  # generator expression
     return HttpResponse(total)
 
+
+def hello(request, name, age):
+    return HttpResponse('안녕하세요. {}. {}살이시네요. :)'.format(name, age))
+
