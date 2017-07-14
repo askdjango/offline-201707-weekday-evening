eba773bc5523b47f5d76839db7c2d7e6f616e61e
Chinseok Lee <allieuslee@gmail.com>
2017-07-14 21:43:26 +0900

    blog.models.Post 모델의 name 필드를 title 로 변경

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     author = models.CharField(max_length=20)
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
