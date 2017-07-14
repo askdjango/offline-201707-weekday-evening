e6f36d84c39bff79582eeeac8a614a3992a01fa3
Chinseok Lee <allieuslee@gmail.com>
2017-07-14 21:55:49 +0900

    blog.views.post_list 뷰에서 title/content 필드에 대해 OR 검색 수행

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from .models import Post
 
@@ -7,7 +8,8 @@ def post_list(request):
 
     query = request.GET.get('query', '')
     if query:
-        qs = qs.filter(title__icontains=query)
+        condition = Q(title__icontains=query) | Q(content__icontains=query)
+        qs = qs.filter(condition)
 
     return render(request, 'blog/post_list.html', {
         'post_list': qs,
