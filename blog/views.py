aa5458581bbd4ea4d212be3317b3fac87eaadfdf
Chinseok Lee <allieuslee@gmail.com>
2017-07-17 22:08:37 +0900

    ifchanged 예시 추가

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db.models import Q
 from django.shortcuts import render
 from .models import Post
@@ -11,9 +12,15 @@ def post_list(request):
         condition = Q(title__icontains=query) | Q(content__icontains=query)
         qs = qs.filter(condition)
 
+    date_list = []
+    for i in range(365):
+        date = datetime.datetime(2017, 1, 1) + datetime.timedelta(days=i)
+        date_list.append(date)
+
     return render(request, 'blog/post_list.html', {
         'post_list': qs,
         'query': query,
+        'date_list': date_list,
     })
 
 
