f19a052c31c5b05e9779f69e5b19539611d8d9bd
Chinseok Lee <allieuslee@gmail.com>
2017-07-21 21:55:04 +0900

    blog.models.Post 모델의 photo 인자에 함수 upload_to 인자 지정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+import os
+import uuid
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse
@@ -8,13 +10,20 @@ def min_length_3(value):
         raise ValidationError('3글자 이상 입력해주세요.')
 
 
+def random_upload_to(instance, filename):
+    ext = os.path.splitext(filename)[-1].lower()  # .jpg
+    filename = uuid.uuid4().hex + ext
+    filepath = os.path.join(filename[:3], filename[3:6], filename[6:9], filename[9:])
+    return filepath
+
+
 class Post(models.Model):
     author = models.CharField(max_length=20, help_text='글쓴이 이름을 입력해주세요.')
     title = models.CharField(max_length=100,
             validators=[min_length_3],
             help_text='제목은 간결하게!!')
     content = models.TextField()
-    photo = models.ImageField(upload_to='blog/post/%Y/%m/%d/%H')
+    photo = models.ImageField(upload_to=random_upload_to)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
