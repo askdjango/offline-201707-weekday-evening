82b4b0e105a1b9fe6f04e498656b49a547cf0573
Chinseok Lee <allieuslee@gmail.com>
2017-07-21 20:30:00 +0900

    blog.models.Post모델에 photo 이미지필드 추가

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
             validators=[min_length_3],
             help_text='제목은 간결하게!!')
     content = models.TextField()
+    photo = models.ImageField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
