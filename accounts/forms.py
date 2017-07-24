7cdf07c0620b3ee3682319b5e8334d7a8b16ea0f
Chinseok Lee <allieuslee@gmail.com>
2017-07-24 22:01:35 +0900

    로그인 answer 유효성검사를 clean_를 통해서 구현

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,10 +7,18 @@ class SignupForm(UserCreationForm):
         fields = UserCreationForm.Meta.fields + ('email',)
 
 
+'''
 def check_answer(value):
     if value != 6:
         raise forms.ValidationError('땡~!')
+'''
 
 class LoginForm(AuthenticationForm):
-    answer = forms.IntegerField(help_text='3+3=?', validators=[check_answer])
+    answer = forms.IntegerField(help_text='3+3=?') #, validators=[check_answer])
+
+    def clean_answer(self):
+        answer = self.cleaned_data.get('answer', 0)
+        if answer != 6:
+            raise forms.ValidationError('땡~!')
+        return answer
 
