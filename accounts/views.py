ff988105e07285841df5c6bdc346a9601ce1d6fa
Chinseok Lee <allieuslee@gmail.com>
2017-07-26 20:35:22 +0900

    django-allauth provider 목록을 login.html에 노출

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
+from allauth.socialaccount.models import SocialApp
+from allauth.socialaccount.templatetags.socialaccount import get_providers
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
-from .forms import SignupForm, ProfileForm
+from django.contrib.auth.views import login as auth_login
+from .forms import SignupForm, ProfileForm, LoginForm
 from .models import Profile
 
 
@@ -16,6 +19,25 @@ def signup(request):
     return render(request, 'accounts/signup_form.html', {
         'form': form,
     })
+
+
+def login(request):
+    provider_list = []
+
+    for provider in get_providers():
+        try:
+            # social_app속성은 provider에는 없는 속성입니다. 그래서 다음과 같이 커스텀 속성을 정의해봤습니다.
+            # 세팅된 Provider별 설정현황을 social\_app 속성으로 지정해보겠습니다.
+            provider.social_app = SocialApp.objects.get(provider=provider.id, sites=settings.SITE_ID)
+        except SocialApp.DoesNotExist:
+            # Provider 설정이 없을 경우 .social_app을 None으로 설정
+            provider.social_app = None
+        provider_list.append(provider)
+
+    return auth_login(request, authentication_form=LoginForm,
+            extra_context={
+                'provider_list': provider_list,
+            })
 
 
 @login_required
