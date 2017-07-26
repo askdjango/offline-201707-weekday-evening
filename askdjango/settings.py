0ffaff5e77da4a7609b7be19f136b1e4162c8d11
Chinseok Lee <allieuslee@gmail.com>
2017-07-26 19:53:44 +0900

    django-storages 연동

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap3',
     'debug_toolbar',
+    'storages',
     'accounts',
     'blog',
     'dojo',
@@ -125,6 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_STORAGE = 'askdjango.storages.StaticS3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'askdjango.storages.MediaS3Boto3Storage'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'askdjango', 'static'),
@@ -135,4 +139,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 INTERNAL_IPS = ['127.0.0.1']
+
+
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = 'askdjango-weekday-evening'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
 
